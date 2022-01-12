246fac4a960775f30910a854c87185d4f53375ad
Jou Xiong <jouxiong15@gmail.com>
2022-01-12 11:54:35 -0600

    added views

--- points/views.py
+++ points/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Payer, Transaction
+from .forms import PayerCreateForm, PayerUpdateForm, TransactionCreateForm
 
-# Create your views here.
+
+class PayerCreate(CreateView):
+    model = Payer
+    template_name = 'payer_create_form.html'
+    form_class = PayerCreateForm
+    
+    
+class PayerUpdate(UpdateView):
+  model = Payer
+  template_name = "payer_update_form.html"
+  form_class = PayerUpdateForm
+
+
+class PayerDelete(DeleteView):
+  model = Payer
+  template_name = "payer_delete_form.html"
+  success_url = "/"
+  
+  
+class TransactionCreate(CreateView):
+    model = Transaction
+    template_name = 'payer_create_form.html'
+    form_class = TransactionCreateForm
