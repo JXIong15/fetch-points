8013750bf1c8997bc7716ef587c3d3ec3d6d01ac
Jou Xiong <jouxiong15@gmail.com>
2022-01-12 12:02:19 -0600

    added forms

---
+++ points/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Payer, Transaction
+
+
+class PayerCreateForm(forms.ModelForm):
+    class Meta:
+        model = Payer
+        fields = ('name', 'total_points')
+
+
+class PayerUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Payer
+        fields = ('name', 'total_points')
+        
+        
+class TransactionCreateForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ('payer', 'points', 'timestamp')
