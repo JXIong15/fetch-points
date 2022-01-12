261c1bc9072549b057d284ff08b226261bb0ae17
Jou Xiong <jouxiong15@gmail.com>
2022-01-12 11:35:46 -0600

    added models

--- points/models.py
+++ points/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Payer(models.Model):
+    name = models.CharField(max_length=255)
+    total_points = models.PositiveIntegerField(default=0)
+    
+
+class Transaction(models.Model):
+    payer = models.ForeignKey(
+        'payer',
+        on_delete=models.CASCADE
+    )
+    points = models.PositiveIntegerField()
+    timestamp = models.DateTimeField()
